44b1296834f61305fd8170e46042659856865e2d
Daniel Ruiz Perez <AV-31273@multiempleos.com.co.local>
2024-12-30 09:38:16 -0500

    Correccion de datos

--- script.js
+++ script.js
@@ -42,17 +42,25 @@ document.getElementById('generatePdf').addEventListener('click', function () {
         image.src = photoData;
         image.onload = function () {
             const photoX = (8 - 2.5) / 2 - 1.5; 
-            doc.addImage(photoData, 'JPEG', photoX, 1.5, 2.5, 2.5); 
+            doc.addImage(photoData, 'JPEG', photoX, 0.9, 2.5, 3); 
 
             doc.setFontSize(10);
 
             doc.setFont("helvetica", "bold");
             doc.setTextColor(100, 149, 237); 
-            doc.text(`${fullName}`, 1.4, 4.5);
+            
+            const pageWidth = doc.internal.pageSize.getWidth();
+            const fullNameWidth = doc.getTextWidth(fullName);
+            const lastNameWidth = doc.getTextWidth(lastName);
+            
+            const fullNameX = (pageWidth - fullNameWidth) / 2;
+            const lastNameX = (pageWidth - lastNameWidth) / 2;
+
+            doc.text(fullName, fullNameX, 4.5);
             
             doc.setFont("helvetica", "normal");
             doc.setTextColor(0, 0, 0);
-            doc.text(`${lastName}`, 1.6, 5); 
+            doc.text(lastName, lastNameX, 5); 
             doc.text(`CC: ${cc}`, 1.2, 5.5); 
             doc.text(`RH: ${rh}`, 1.7, 6); 
             doc.text(`ARL: ${arl}`, 1.5, 6.5);
@@ -65,7 +73,7 @@ document.getElementById('generatePdf').addEventListener('click', function () {
 
             doc.addPage();
 
-                const longText = ` Este documento identifica al portador
+            const longText = ` Este documento identifica al portador
         como empleado en misión de
                 MULTIEMPLEOS S.A.   
                 
@@ -84,8 +92,6 @@ document.getElementById('generatePdf').addEventListener('click', function () {
 
 
 
-
-
                        ARL SURA:
     018000511414 O  018000941414
 
@@ -97,20 +103,20 @@ document.getElementById('generatePdf').addEventListener('click', function () {
             const textWidth = 7.5; 
             const textLines = doc.splitTextToSize(longText, textWidth);
 
-        const logo = 'ico/logosura.png';    
-        const bgImage = 'ico/logome2.png';
+            const logo = 'ico/logosura.png';    
+            const bgImage = 'ico/logome2.png';
 
-        try {
-            doc.addImage(logo, 'PNG', 1.7, 3.5, 2.5, 1.2);
-        } catch (error) {
-            console.error('Error al agregar el fondo: ', error);
-        }
+            try {
+                doc.addImage(logo, 'PNG', 1.7, 3.5, 2.5, 1.2);
+            } catch (error) {
+                console.error('Error al agregar el fondo: ', error);
+            }
 
-        try {
-            doc.addImage(bgImage, 'PNG', 1.6, 7, 2, 0.6);
-        } catch (error) {
-            console.error('Error al agregar el logo: ', error);
-        }
+            try {
+                doc.addImage(bgImage, 'PNG', 1.6, 7, 2, 0.6);
+            } catch (error) {
+                console.error('Error al agregar el logo: ', error);
+            }
         
             doc.setFontSize(6);
 
